ad842f48a67f33be9b3a6e2b1c467931d8adaf7d
adisve <adis.veletanlic@gmail.com>
2023-03-31 17:15:31 +0200

    Finish

--- src/main.py
+++ src/main.py
@@ -1,57 +1,50 @@
-import re
-import os
 import sys
-from googletrans import Translator
+from translate import translate
 
-# Get the project path, language code, and output file path from the command line arguments
-if len(sys.argv) != 4:
-    print("Usage: python script.py <project_path> <language_code> <output_file_path>")
-    sys.exit(1)
-project_path = sys.argv[1]
-language_code = sys.argv[2]
-output_file_path = sys.argv[3]
+def get_user_choice(language_code: str, project_path: str, output_file_path: str):
+    
+    print("Please choose an option:")
+    print(f"1. Set language code\tCurrent: {language_code}")
+    print(f"2. Set project path\tCurrent: {project_path}")
+    print(f"3. Set output file path\tCurrent: {output_file_path}")
+    print(f"4. Translate")
+    print("5. Exit")
+    
+    # Get user input and validate it
+    while True:
+        user_input = input("\nEnter your choice (1-5) >> ")
+        if user_input.isdigit() and int(user_input) in range(1, 6):
+            return int(user_input)
+        else:
+            print("Invalid input. Please enter a number between 1 and 5.")
 
-# Regular expression pattern to match NSLocalizedString instances
-pattern = re.compile(r'NSLocalizedString\("([^"]+)"')
+# Main function to run the program
+def main():
+    
+    language_code = "not set"
+    output_file_path = "not set"
+    project_path = "not set"
+    
+    print("\nWelcome to SwiftLocalizationGenerator\n\n")
+    
+    while True:
+        # Display the menu and get user input
+        choice = get_user_choice(language_code=language_code, project_path=project_path, output_file_path=output_file_path)
+        
+        # Call the appropriate function based on the user's choice
+        if choice == 1:
+            language_code = input(">> ")
+        elif choice == 2:
+            project_path = input(">> ")
+        elif choice == 3:
+            output_file_path = input(">> ")
+        elif choice == 4:
+            translate(project_path, language_code, output_file_path)
+        else:
+            # Exit the program
+            print("Exiting...")
+            sys.exit()
 
-translator = Translator()
-
-# Set to store processed keys
-processed_keys = set()
-
-# List to store all the NSLocalizedString keys and values
-keys_values = []
-
-# Walk through the project directory and find all .m and .swift files
-for root, dirs, files in os.walk(project_path):
-    for file in files:
-        print(f"Processing file: {file}")
-        if file.endswith(".m") or file.endswith(".swift"):
-            file_path = os.path.join(root, file)
-            with open(file_path, "r") as f:
-                contents = f.read()
-                # Search for all NSLocalizedString instances in the file
-                matches = pattern.findall(contents)
-                for match in matches:
-                    # Extract the key from the NSLocalizedString instance
-                    key = match
-                    if key in processed_keys:
-                        continue
-                    # Translate the key to the specified language
-                    try:
-                        translation = translator.translate(key, src='en', dest=language_code).text
-                        # Replace double quotes with single quotes
-                        translation = translation.replace('"', "'").capitalize()
-                    except Exception as e:
-                        print(f"Error translating key '{key}': {e}")
-                        continue
-                    keys_values.append((key, translation))
-                    processed_keys.add(key)
-
-# Write the keys and values to the output text file
-try:
-    with open(output_file_path, "w") as f:
-        for key, value in keys_values:
-            f.write(f'"{key}"="{value}";\n\n')
-except Exception as e:
-    print(f"Error writing to file '{output_file_path}': {e}")
+# Call the main function to run the program
+if __name__ == "__main__":
+    main()
